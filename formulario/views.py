145d795e385f05a65f938ad616ff7c2135c948b0
duridev <duridev@outlook.com>
2026-02-26 22:57:56 -0300

    Add formulario_view to handle contact form submissions

---
+++ formulario/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .forms import ContactForm
+
+def formulario_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data["nombre"]
+            correo = form.cleaned_data["correo"]
+            mensaje = form.cleaned_data["mensaje"]
+            
+            return render(request, "formulario/exito.html", {"nombre": nombre})
+    else:
+        form = ContactForm()
+    
+    return render(request, "formulario/formulario.html", {"form": form})
+
+# Create your views here.
